50b9adfc70c776192ca9e05e9f30b4968918dcdb
96glory <click_syk@naver.com>
2022-10-21 20:34:48 +0900

    LocalStorage를 일기 데이터베이스로 사용하기

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -16,7 +16,7 @@ const DiaryEditor = ({ isEdit, originData }) => {
   const [emotion, setEmotion] = useState(3);
   const [date, setDate] = useState(getStringDate(new Date()));
 
-  const { onCreate, onEdit } = useContext(DiaryDispatchContext);
+  const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
 
   const handleClickEmote = (emotion) => {
     setEmotion(emotion);
@@ -39,6 +39,13 @@ const DiaryEditor = ({ isEdit, originData }) => {
     navigate('/', { replace: true }); // 뒤로가기를 눌렀을 때 edit 모드로 돌아오지 못하게끔
   };
 
+  const handleRemove = () => {
+    if (window.confirm('정말 삭제하시겠습니까?')) {
+      onRemove(originData.id);
+      navigate('/', { replace: true });
+    }
+  };
+
   useEffect(() => {
     if (isEdit) {
       setDate(getStringDate(new Date(parseInt(originData.date))));
@@ -52,6 +59,7 @@ const DiaryEditor = ({ isEdit, originData }) => {
       <MyHeader
         headText={isEdit ? '일기 수정하기' : '새 일기 쓰기'}
         leftChild={<MyButton text={'< 뒤로가기'} onClick={() => navigate(-1)} />}
+        rightChild={isEdit && <MyButton text={'삭제하기'} type={'negative'} onClick={handleRemove} />}
       />
       <div>
         <section>
